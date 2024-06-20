35e360b0215df92fd8bb3ea0540dcfdff29093a2
ian-alemida <iangtaonline1@gmail.com>
2024-06-20 01:48:59 -0300

    devinindo bg

---
+++ bg-stars/src/App.jsx
@@ -0,0 +1,19 @@
+import { styled } from "styled-components"
+
+const BackgroundBlack = styled.div`
+  background-color: #000;
+  width: 100%;
+  margin: 0;
+  height: 100vh;
+`
+
+function App() {
+
+  return (
+    <BackgroundBlack>
+      <h1>ola</h1>
+    </BackgroundBlack>
+  )
+}
+
+export default App
