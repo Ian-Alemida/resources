8fe060e558731eb233d1280c2795b237308a79a5
ian-alemida <iangtaonline1@gmail.com>
2024-06-20 02:19:42 -0300

    definindo bg em espiral

--- bg-stars/src/App.jsx
+++ bg-stars/src/App.jsx
@@ -1,17 +1,21 @@
 import { styled } from "styled-components"
 
 const BackgroundBlack = styled.div`
-  background-color: #000;
+ background: radial-gradient(ellipse closest-side at center, #112949 0, #0b1a2c 36%, #000 100%);
   width: 100%;
   margin: 0;
   height: 100vh;
+  position: relative;
+`
+const Stars = styled.div`
+  
 `
 
 function App() {
 
   return (
     <BackgroundBlack>
-      <h1>ola</h1>
+      <Stars></Stars>
     </BackgroundBlack>
   )
 }
