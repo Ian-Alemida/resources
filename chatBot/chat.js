939dbe0e4d7b482c54fb19236ce3625be470de65
ian-alemida <iangtaonline1@gmail.com>
2024-07-07 23:25:55 -0300

    ligando o chatbot ao front-end

--- chatBot/chat.js
+++ chatBot/chat.js
@@ -4,31 +4,34 @@ dotenv.config();
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-async function run() {
+export async function execultaChat(mensagem) {
   const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro" });
 
   const chat = model.startChat({
     history: [
       {
         role: "user",
-        parts: [{ text: "Oi, adoro viajar!" }],
+        parts: [
+          {
+            text: "Você é JordIA, um chatbot amigável que representa a empresa jornada viagens. você pode responder mensagens referentes a pacotes turísticos, viagens e destinos diversos",
+          },
+        ],
       },
       {
         role: "model",
-        parts: [{ text: "Ah, que legal. Para onde deseja viajar?" }],
+        parts: [
+          {
+            text: "Olá! Obrigada por entrar em contato com o Jornada viagens. Antes de responder suas duvidas pode me informar sue nome??",
+          },
+        ],
       },
     ],
     generationConfig: {
-      maxOutputTokens: 100,
+      maxOutputTokens: 1000,
     },
   });
 
-  const msg = "Quero ir para a Etiópia, em busca da arca da aliança";
-
-  const result = await chat.sendMessage(msg);
+  const result = await chat.sendMessage(mensagem);
   const response = await result.response;
-  const text = response.text();
-  console.log(text);
+  return response.text();
 }
-
-run();
